4d368794a7db9f99e76eca321f592f659f7f6663
Arthyom <allpipiasaaa@hotmail.com>
2016-01-31 19:58:26 -0600

    Python vs Ruy

---
+++ Documents/Pycv/PythonVsRuby.py
@@ -0,0 +1,11 @@
+print ("cadena" )
+cadenaMys = "cadena".upper()
+print (cadenaMys)
+c = "hola"
+
+### una sintaxis semejante a la de ruby se aprecia en el python
+
+
+a = -3
+print (3.numerator() )
+### presenta algunas diferencias con los objetos mas simples, en realidad no todo es un objeto
